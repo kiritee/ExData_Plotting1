ff83b88ee04b9a39881ba5acc120d514a1f8d0aa
Kiritee <konark@gmail.com>
2015-08-10 01:36:03 +0100

    added data prep code

---
+++ clean_data.R
@@ -0,0 +1,13 @@
+CleanData<-function(){
+    test<-read.table("household_power_consumption.txt",sep=";",nrows=100,na.strings="?",header=TRUE)
+    classes<-sapply(test,class)
+    classes[1:2]<-"character"
+    df<-read.table("household_power_consumption.txt",sep=";",na.strings="?",header=TRUE,colClasses=classes)
+    Datetime<-paste(df$Date,df$Time,sep=" ")
+    Datetime<-as.POSIXct(strptime(Datetime,"%d/%m/%Y %H:%M:%S"))
+    d1<-as.POSIXct("2007-01-02")
+    d2<-as.POSIXct("2007-02-02")
+    indices<-Datetime3>=d1 & Datetime3<=d2
+    n<-ncol(df)
+    df<-cbind(Datetime[indices],df[indices,3:n])
+}    
