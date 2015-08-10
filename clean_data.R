bffc0e38d205ffe0dbd1353ad0600e0e3d425a9c
Kiritee <konark@gmail.com>
2015-08-10 02:16:17 +0100

    modified plot1

--- clean_data.R
+++ clean_data.R
@@ -5,9 +5,11 @@ CleanData<-function(){
     df<-read.table("household_power_consumption.txt",sep=";",na.strings="?",header=TRUE,colClasses=classes)
     Datetime<-paste(df$Date,df$Time,sep=" ")
     Datetime<-as.POSIXct(strptime(Datetime,"%d/%m/%Y %H:%M:%S"))
-    d1<-as.POSIXct("2007-01-02")
+    d1<-as.POSIXct("2007-02-01")
     d2<-as.POSIXct("2007-02-02")
-    indices<-Datetime3>=d1 & Datetime3<=d2
+    indices<-Datetime>=d1 & Datetime<=d2
+    days<-weekdays(Datetime,TRUE)
     n<-ncol(df)
-    df<-cbind(Datetime[indices],df[indices,3:n])
+    df2<-cbind(days[indices],df[indices,3:n])
+    colnames(df2)[1]<-"Days"
 }    
