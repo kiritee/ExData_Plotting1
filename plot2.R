27c0d74ae008e89e94ba2cf4a2f11d0a654b88fd
Kiritee <konark@gmail.com>
2015-08-10 03:20:45 +0100

    Plot 3 Final

--- plot2.R
+++ plot2.R
@@ -1,1 +1,4 @@
 #plot 2
+png(filename="plot2.png")
+plot(df2$Datetime,df2$Global_active_power,type="l",ylab="Global Active Power (kilowatts)", xlab="")
+dev.off()
--- plot3.R
+++ plot3.R
@@ -1,1 +1,8 @@
 # plot 3
+png(filename = "plot3.png")
+plot(df2$Datetime,df2$Sub_metering_1,type="n",ylab="Energy sub metering",xlab="")
+lines(df2$Datetime,df2$Sub_metering_1,col="black")
+lines(df2$Datetime,df2$Sub_metering_2,col="red")
+lines(df2$Datetime,df2$Sub_metering_3,col="blue")
+legend("topright", lty="solid", col = c("black","blue", "red"), legend = c("Sub-Metering 1", "Sub-Metering 2", "Sub-Metering 3"))
+dev.off()
