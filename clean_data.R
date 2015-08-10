c697df701e7dfeaa0e4483bb7f2be265327aeb14
Kiritee <konark@gmail.com>
2015-08-10 02:58:34 +0100

    Plot 1 Final

--- clean_data.R
+++ clean_data.R
@@ -6,10 +6,10 @@ CleanData<-function(){
     Datetime<-paste(df$Date,df$Time,sep=" ")
     Datetime<-as.POSIXct(strptime(Datetime,"%d/%m/%Y %H:%M:%S"))
     d1<-as.POSIXct("2007-02-01")
-    d2<-as.POSIXct("2007-02-02")
-    indices<-Datetime>=d1 & Datetime<=d2
-    days<-weekdays(Datetime,TRUE)
+    d2<-as.POSIXct("2007-02-03")
+    indices<-!is.na(Datetime) & Datetime>=d1 & Datetime<d2
+    #days<-weekdays(Datetime,TRUE)
     n<-ncol(df)
-    df2<-cbind(days[indices],df[indices,3:n])
-    colnames(df2)[1]<-"Days"
+    df2<-cbind(Datetime[indices],df[indices,3:n])
+    colnames(df2)[1]<-"Datetime"
 }    
